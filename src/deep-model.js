121422ac75e7ddea7e75a696b29b79cf46a65562
Dallas Pool @codeninja <codeninja@gmail.com>
2024-07-26 15:09:44 -0500

    lint

--- src/deep-model.js
+++ src/deep-model.js
@@ -3,16 +3,16 @@
  */
 
 ; (function (factory) {
-  // if (typeof define === 'function' && define.amd) {
-  //     // AMD
-  //     define(['underscore', 'backbone'], factory);
-  // } else if (typeof exports === 'object') {
-  //     // CommonJS
-  //     module.exports = factory(require('underscore'), require('backbone'));
-  // } else {
-  // always use webpack globals
-  factory(_, Backbone);
-  // }
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['underscore', 'backbone'], factory);
+  } else if (typeof exports === 'object') {
+    // CommonJS
+    module.exports = factory(require('underscore'), require('backbone'));
+  } else {
+    // globals
+    factory(_, Backbone);
+  }
 }(function (_, Backbone) {
 
   /**
